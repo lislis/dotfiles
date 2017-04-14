4872671ed075348f4ed59003cd7a586d3e97d107
lislis <lislis@users.noreply.github.com>
2017-04-14 23:03:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,23 +4,23 @@ echo "Check for updates"
 sudo apt-get update
 
 echo "Installing git and zsh emacs"
-sudo apt-get install git zsh emacs --y
+sudo apt-get install git zsh emacs -y
 
 echo "Configure git"
 read -p 'Git user name: ' gituser
 read -p 'Git email: ' gitmail
 read -p 'Git editor: ' giteditor
 
-git config --global user.name gituser
-git config --global user.email gitmail
-git config --global core.editor giteditor
+git config --global user.name $gituser
+git config --global user.email $gitmail
+git config --global core.editor $giteditor
 
 echo "generating ssh key"
 read -p 'Email for ssh key: ' sshmail
-ssh-keygen -t rsa -b 4096 -C sshmail
+ssh-keygen -t rsa -b 4096 -C $sshmail
 
 echo "installing more packages"
-sudo apt-get install terminator cmus clementine owncloud-client shutter keepassx openjdk-8-jre calibre ppa-purge wget --y
+sudo apt-get install terminator cmus clementine owncloud-client shutter keepassx openjdk-8-jre calibre ppa-purge wget -y
 
 echo "Cloning emacs.d"
 git clone https://github.com/lislis/prelude.git ~/.emacs.d
