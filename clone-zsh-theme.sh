5d60e8b84b90033b2c5f7ada40f09573bf1c854c
lislis <lislis@users.noreply.github.com>
2018-03-20 23:52:01 +0100

    Create clone-zsh-theme.sh

---
+++ clone-zsh-theme.sh
@@ -0,0 +1,2 @@
+echo "Cloning spezi theme"
+wget -o ~/.oh-my-zsh/custom/themes/spezi.zsh-theme "https://gist.githubusercontent.com/lislis/0be6da335835de6af7bae92e70b7901f/raw/657a2e46379badc197d484cf46492467ce4dfac7/spezi.zsh-theme"
