8ad6d73ae433a67ea179acf00f465bca93cf49cd
lislis <lislis@users.noreply.github.com>
2018-03-21 00:10:24 +0100

    Update and rename clone-zsh-theme.sh to copy.sh

--- copy.sh
+++ copy.sh
@@ -1,8 +1,9 @@
-echo "Cloning spezi theme"
+echo "Copying spezi theme"
 # "https://gist.githubusercontent.com/lislis/0be6da335835de6af7bae92e70b7901f"
+mkir -p ~/.oh-my-zsh/custom/themes
 cp spezi.zsh-theme ~/.oh-my-zsh/custom/themes/spezi.zsh-theme 
 
-echo "Cloning .zshrc"
+echo "Copying .zshrc"
 cp .zshrc ~/.zshrc
 
 echo "so far so good!"
