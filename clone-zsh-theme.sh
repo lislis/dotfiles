9792a463bffcd855bfeb555172daab0859f769f0
lislis <lislis@users.noreply.github.com>
2018-03-21 00:04:34 +0100

    Update clone-zsh-theme.sh

--- clone-zsh-theme.sh
+++ clone-zsh-theme.sh
@@ -1,3 +1,8 @@
 echo "Cloning spezi theme"
 # "https://gist.githubusercontent.com/lislis/0be6da335835de6af7bae92e70b7901f"
 cp spezi.zsh-theme ~/.oh-my-zsh/custom/themes/spezi.zsh-theme 
+
+echo "Cloning .zshrc"
+cp .zshrc ~/.zshrc
+
+echo "so far so good!"
