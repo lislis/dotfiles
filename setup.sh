387dfc5c39b43a9720483ac3c4558b0b94657a8f
lislis <lislis@users.noreply.github.com>
2018-04-02 22:22:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
-echo "Welcome to the setup script. Fingers crossed!"
+echo "Welcome to the setup script. Fingers crossed!/n/n"
 
-echo "Check for updates"
+echo "Check for updates/n/n"
 sudo apt update
 
-echo "Addiing ppas"
+echo "Addiing ppas/n/n"
 sudo add-apt-repository ppa:phoerious/keepassxc
 sudo add-apt-repository ppa:nextcloud-devs/client
 
 sudo apt update
 
-echo "Installing software packages"
+echo "Installing software packages/n/n"
 sudo apt install git zsh emacs terminator clojure openjdk-8-jre ppa-purge wget curl vlc ruby-ffi autoconf libtool postgresql postgresql-contrib libpq-dev libsqlite3-dev -y
 sudo apt install cmus clementine inkscape nextcloud-client shutter keepassxc calibre -y
 
 
-echo "Configure git"
+echo "Configure git/n"
 read -p 'Git user name: ' gituser
 read -p 'Git email: ' gitmail
 read -p 'Git editor: ' giteditor
@@ -23,11 +23,11 @@ git config --global user.name $gituser
 git config --global user.email $gitmail
 git config --global core.editor $giteditor
 
-echo "generating ssh key"
+echo "generating ssh key/n/n"
 read -p 'Email for ssh key: ' sshmail
 ssh-keygen -t rsa -b 4096 -C $sshmail
 
-echo "Cloning emacs.d"
+echo "Cloning emacs.d/n/n"
 git clone https://github.com/lislis/prelude.git ~/.emacs.d
 
 echo "Installing Rust/n/n"
@@ -41,7 +41,16 @@ cd ~/.rbenv && src/configure && make -C src
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo "rbenv is installed but still needs to be configured!!!/n/n"
 
-echo "installing oh my zsh"
+echo "Installing Node/n/n"
+# https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo "Node installed, but check the notes in the script/n/n"
+# @todo configure path
+
+# @todo install pip and virtual env
+
+echo "installing oh my zsh/n/n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "so far so good!"
+echo "/n/nSo far so good!/n/n"
